910c6ab165d76b9eb35975c0b7f75bbaaac513f5
ErinsonDLC <angeloe2811@gmail.com>
2022-06-01 13:39:01 -0400

    Hi

---
+++ Exercise1/Excercise1Solution/Excercise1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un numero:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            int[] array = new int[n];
+            int producto = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Random rand = new Random();
+                array[i] = rand.Next(20); // Set value limit to 20
+
+                producto *= array[i];
+            }
+            Console.WriteLine("\nLa multiplicacion es igual a: " + producto);
+
+            if (producto % 2 == 0)
+            {
+                int sum = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+
+                Console.WriteLine("\nLa suma total es: " + sum);
+            }
+            else { Console.WriteLine("\n0"); }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Exercise2/Excercise2Solution/Excercise2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux = ""; // auxiliar var
+
+            Console.WriteLine("Digite el PIN de seguridad: ");
+            string pin = Console.ReadLine();
+
+            foreach (var number in pin)
+            {
+                bool num = int.TryParse(number.ToString(), out int n); // returns true when it can be parso to int
+
+                if (num == true)
+                {
+                    // save numbers in auxiliar var
+                    aux += number;
+                }
+            }
+
+            PinValidation(aux);
+
+            Console.Read();
+        }
+
+        // Pin validation method
+        static void PinValidation(string pin)
+        {
+            if (pin.Count() == 4 || pin.Count() == 6)
+            {
+                bool verify = false;
+
+                // Separe the string and count how many times every char appear
+                foreach (var number in pin.GroupBy(p => p).Where(p => p.Count() != 1))
+                {
+                    // Assing true only if a char repeats
+                    verify = true;
+                }
+
+                if (verify == true)
+                {
+                    Console.WriteLine("\n" + pin + " - Pin Invalido");
+                }
+                else { Console.WriteLine(pin + " - Pin Valido"); }
+            }
+            else { Console.WriteLine(pin + " - Pin Invalido"); }
+        }
+    }
+}
